7f4c37d45700e87a5987a4e8fa6ae6b1de09461e
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-22 23:27:59 +0530

    server setup created with creation logic

---
+++ V.1.0.0/src/setups/query/server.query.setup.js
@@ -0,0 +1,27 @@
+const { RQuery, checkSchema } = require('../database/mysql.setup');
+const { info } = require('../logger/logging.setup')
+
+const dbValidation = async () => {
+    var Tables_in_base = await RQuery(`
+        show tables from ${process.env.databaseMySQL};
+    `)
+    if (Tables_in_base.length >= 1) {
+        info(data = {
+            "funcName": "dbValidation()",
+            "msg": "All required tables exists",
+            "status": "OK",
+            "statusCode": 201
+        })
+    } else {
+        await checkSchema()
+        info(data = {
+            "funcName": "dbValidation()",
+            "msg": "Some tables missing, create function initiated",
+            "status": "OK",
+            "statusCode": 201
+        })
+    }
+    return;
+}
+
+module.exports = dbValidation;
