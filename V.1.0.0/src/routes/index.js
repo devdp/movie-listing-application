c2781d76f9c4d29c5ddea669af0c88686f6fc441
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 03:15:12 +0530

    route name consistency improved

--- V.1.0.0/src/routes/index.js
+++ V.1.0.0/src/routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const onboard = require('./onboard');
+const onboard = require('./onboard/onboard.route');
 const login = require('./login/login.route');
 const jwt = require('./jwt/jwt.route')
-// const dashboard = require('./dashboard');
+const dashboard = require('./dashboard/dashboard.route');
+const jwtMiddleware = require('../middleware/jwt/jwt.middleware')
 
 router.use('/onboard', onboard);
 router.use('/login', login);
 router.use('/token', jwt);
-// router.use('/dashboard', dashboard);
+router.use('/dashboard', [jwtMiddleware], dashboard);
 
 module.exports = router;
