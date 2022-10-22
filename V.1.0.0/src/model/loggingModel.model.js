adcd116a13808f0b84f210ccf7be0ea9cca7388a
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-22 23:31:16 +0530

    logging model file created

---
+++ V.1.0.0/src/model/loggingModel.model.js
@@ -0,0 +1,16 @@
+const pino = require('pino')
+
+const logger = () => {
+    logging = pino({
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true
+            }
+        }
+    })
+
+    return logging
+}
+
+module.exports = logger;
