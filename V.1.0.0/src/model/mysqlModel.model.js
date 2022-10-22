858a8eed4963c3132fca6fc23ad2d420f6810ba3
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 03:19:52 +0530

    mysql model -> added movie schema

--- V.1.0.0/src/model/mysqlModel.model.js
+++ V.1.0.0/src/model/mysqlModel.model.js
@@ -6,7 +6,17 @@ const models = {
                 email varchar(50),
                 username varchar(50),
                 password varchar(100)
-            )`
+            );`,
+    moviesListing: `
+        CREATE TABLE movies
+            (
+                id int auto_increment primary key,
+                movieName varchar(100),
+                rating int,
+                cast text,
+                genre varchar(50),
+                releaseDate timestamp   
+            );`
     };
 
 module.exports = models;
