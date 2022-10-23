444a17d4b85a77f37e20baf86bc7a38f98206686
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 14:41:24 +0530

    dashbaord queries created

--- V.1.0.0/src/setups/query/dashboard.query.setup.js
+++ V.1.0.0/src/setups/query/dashboard.query.setup.js
@@ -1,20 +1,143 @@
 const { error, info } = require('../logger/logging.setup');
 const { RQuery, WQuery } = require('../database/mysql.setup');
 
-const create = (req, res) => {
+const create = async (req) => {
+    const {
+        movieName,
+        rating,
+        cast,
+        genre,
+        releaseDateTime
+    } = req.body
+
+    if (movieName == undefined || rating == undefined || cast == undefined || genre == undefined || releaseDateTime == undefined) {
+        return (data = data = {
+            'msg': 'Some Parameteres Missing',
+            'status': "OK",
+            'statusCode': 200
+        })
+    }
+
+    var checkMovie = await RQuery(`
+        SELECT 
+            id 
+        FROM 
+            movies 
+        WHERE 
+            movieName = '${movieName}';`
+    );
+
+    if (checkMovie.length > 0) {
+        return (data = data = {
+            'msg': 'Movie Already Exists',
+            'status': "OK",
+            'statusCode': 200
+        })
+    }
+
+    var response = await WQuery(`
+        INSERT INTO
+            movies
+        (
+            movieName,
+            rating,
+            cast,
+            genre,
+            releaseDate,
+            userId
+        ) VALUES (
+            '${movieName}',
+            '${rating}',
+            '${cast.join(',')}',
+            '${genre}',
+            '${releaseDateTime}',
+            '${req.params.id}'
+        );`
+    )
+
+    return (data = {
+        'msg': response,
+        'status': "OK",
+        'statusCode': 200
+    })
 
 }
 
-const get = (req, res) => {
+const get = async (req) => {
+    var response = await RQuery(`
+        SELECT 
+            id as movieId,
+            movieName,
+            rating,
+            cast,
+            genre,
+            releaseDate
+        FROM
+            movies
+        WHERE
+            userId = ${req.params.id};
+    `);
+
+    return (data = {
+        'data': response,
+        'msg': "Movies Fetched Successfully",
+        'status': "OK",
+        'statusCode': 200
+    });
+}
+
+const update = async (req) => {
+    const {
+        movieName,
+        rating,
+        cast,
+        genre,
+        releaseDateTime,
+        movieId
+    } = req.body;
+
+    if (movieName == undefined || rating == undefined || cast == undefined || genre == undefined || releaseDateTime == undefined || movieId == undefined) {
+        return (data = data = {
+            'msg': 'Some Parameteres Missing',
+            'status': "OK",
+            'statusCode': 200
+        })
+    }
+
+    var response = await WQuery(`
+        UPDATE 
+            movies
+        SET
+            movieName = '${movieName}',
+            rating = '${rating}',
+            cast = '${cast}',
+            genre = '${genre}',
+            releaseDate = '${releaseDateTime}'
+        WHERE
+            id = '${movieId}'
+    `);
+
+    return (data = {
+        'msg': response,
+        'status': "OK",
+        'statusCode': 200
+    });
 
 }
 
-const update = (req, res) => {
+const del = async (req) => {
+    response = await WQuery(`
+        DELETE FROM
+            movies
+        WHERE
+            id = ${req.params.movieId}
+    `);
 
-}
-
-const del = (req, res) => {
-
+    return (data = {
+        'msg': response,
+        'status': "OK",
+        'statusCode': 200
+    })
 }
 
 module.exports = {
