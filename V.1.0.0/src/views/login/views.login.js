9ebcfce8478ca377b417ff857f634e1979fa8333
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 18:45:41 +0530

    login page finalised

---
+++ V.1.0.0/src/views/login/views.login.js
@@ -0,0 +1,37 @@
+const baseUrl = "http://localhost:3000/routes/v1/"
+
+const login = async () => {
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    var settings = {
+        "url": `${baseUrl}login`,
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "email": email,
+            "password": password
+        }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        alert(response.msg)
+        console.log(response)
+        if (response.status == 'OK') {
+            localStorage.setItem('email', response.data.email)
+            localStorage.setItem('username', response.data.fullname)
+            localStorage.setItem('accessToken', response.token.accessToken)
+            localStorage.setItem('refreshToken', response.token.refreshToken)
+            localStorage.setItem('userId', response.data.userId)
+            window.location.replace('http://localhost:3000/routes/v1/dashboard')
+        }
+    });
+}
+
+const signUp = async () => {
+    window.location.replace('http://localhost:3000/routes/v1/onboard')
+}
