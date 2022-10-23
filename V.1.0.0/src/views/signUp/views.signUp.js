cf060795c65af86ad4a93c99e60fc4de28c9469e
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 18:45:59 +0530

    signup page finalised

---
+++ V.1.0.0/src/views/signUp/views.signUp.js
@@ -0,0 +1,31 @@
+const baseUrl = "http://localhost:3000/routes/v1/"
+
+const signUp = async () => {
+
+    const email = document.getElementById('email').value;
+    const fullname = document.getElementById('fullname').value;
+    const password = document.getElementById('password').value;
+
+    var settings = {
+        "url": `${baseUrl}onboard`,
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "email": email,
+            "fullname": fullname,
+            "password": password
+        }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        alert(response.msg)
+        window.location.replace('http://localhost:3000/routes/v1/onboard')
+    });
+}
+
+const login = async () => {
+    window.location.replace('http://localhost:3000/routes/v1/login')
+}
