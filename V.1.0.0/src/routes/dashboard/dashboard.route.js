df90461e1983fe7af245efcb02cfb8b65bf1dc72
devdp8 <bramhesh.srivastav@algo8.ai>
2022-10-23 14:41:02 +0530

    dashboard routes mapped

--- V.1.0.0/src/routes/dashboard/dashboard.route.js
+++ V.1.0.0/src/routes/dashboard/dashboard.route.js
@@ -2,20 +2,24 @@ const { create, get, update, del } = require('../../setups/query/dashboard.query
 
 const route = require('express').Router();
 
-route.post('/create/:id', (req, res) => {
-    create()
+route.post('/create/:id', async (req, res) => {
+    var response = await create(req)
+    res.send(response)
 });
 
 route.get('/list/:id', async (req, res) => {
-    get()
+    var response = await get(req)
+    res.send(response)
 });
 
 route.put('/update/:id', async (req, res) => {
-    update()
+    var response = await update(req)
+    res.send(response)
 });
 
-route.delete('/del/:id', async (req, res) => {
-    del()
+route.delete('/del/:movieId', async (req, res) => {
+    var response = await del(req)
+    res.send(response)
 });
 
 module.exports = route;
